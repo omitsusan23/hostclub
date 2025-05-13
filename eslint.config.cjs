1fbc984bc7d2c927c88d7d73b294c2597a98fb18
omitsusan23 <m0323replay0417@gmail.com>
2025-05-13 18:12:25 +0900

    Deleted eslint.config.cjs

--- eslint.config.cjs
+++
@@ -1,8 +0,0 @@
-// eslint.config.cjs
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
-
-// 既存の .eslintrc.cjs が CommonJS で書かれている設定をそのまま流用
-const legacyConfig = require('./.eslintrc.cjs');
-
-export default legacyConfig;
