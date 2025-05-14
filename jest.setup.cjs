f5e83cd5e4df21c61c210dec3371bec77a46c2f5
omitsusan23 <m0323replay0417@gmail.com>
2025-05-14 17:25:37 +0900

    Updated jest.setup.cjs

--- jest.setup.cjs
+++ jest.setup.cjs
@@ -2,9 +2,12 @@
 
 /**
  * Setup script for Jest environment
- * Polyfill TextEncoder/TextDecoder for JSDOM
+ * 1) Polyfill TextEncoder/TextDecoder for JSDOM
+ * 2) Load @testing-library/jest-dom matchers
  */
 const { TextEncoder, TextDecoder } = require('util');
-
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
+
+// 追加：jest-dom のカスタムマッチャーを登録
+require('@testing-library/jest-dom');
