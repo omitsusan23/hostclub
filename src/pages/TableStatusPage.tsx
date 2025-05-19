bf7232a36732f3cf743b1ddcb08b2370970eb3b1
omitsusan23 <m0323replay0417@gmail.com>
2025-05-20 04:17:53 +0900

    Deploy application to Netlify

--- src/pages/TableStatusPage.tsx
+++ src/pages/TableStatusPage.tsx
@@ -208,7 +208,7 @@ export default function TableStatusPage() {
       </header>
 
       {/* 本文部 */}
-      <main id="main-content" className="overflow-x-hidden container mx-auto px-1.5 py-2 grid grid-cols-3 gap-3">
+      <main id="main-content" className="overflow-x-hidden container mx-auto px-2 py-2 grid grid-cols-3 gap-3">
         {renderedTables}
       </main>
 
