f989ea61d2245a619c2a046328b6b34dda2233a5
omitsusan23 <m0323replay0417@gmail.com>
2025-05-19 23:35:39 +0900

    Deploy application to Netlify

--- src/pages/TableStatusPage.tsx
+++ src/pages/TableStatusPage.tsx
@@ -108,7 +108,7 @@ export default function TableStatusPage() {
           </div>
 
           {/* 詳細部 */}
-<div className="inline-flex items-baseline space-x-1 border-b border-current pb-[2px]">
+<div className="p-2 flex-grow grid grid-cols-[6ch_1fr] gap-x-2 gap-y-1">
   {table.princess ? (
     <>
       <span className="text-[12px] font-semibold">姫名:</span>
