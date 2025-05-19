44c2fc4e9c0f779c5565001ac65ec120bd92e02d
omitsusan23 <m0323replay0417@gmail.com>
2025-05-19 18:08:59 +0900

    Updated Layout.tsx

---
+++ src/components/Layout.tsx
@@ -0,0 +1,18 @@
+// src/components/Layout.tsx
+import React, { ReactNode } from 'react';
+
+type Props = {
+  children: ReactNode;
+};
+
+export const Layout: React.FC<Props> = ({ children }) => {
+  return (
+    <div className="container mx-auto">
+      {/* 例：ここにヘッダーを追加してもOK */}
+      <main className="px-4 sm:px-6 md:px-8">
+        {children}
+      </main>
+      {/* 例：ここにフッターを追加してもOK */}
+    </div>
+  );
+};
