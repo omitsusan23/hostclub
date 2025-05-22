ddb83c38417f85e8a87775663031baeaf693e514
omitsusan23 <m0323replay0417@gmail.com>
2025-05-22 15:18:45 +0900

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { StoreProvider } from './context/StoreContext';
 import Layout from './components/Layout';
-import AppRoutes from './AppRoutes'; // ルーティング定義
+import AppRoutes from './AppRoutes';
 
 const App: React.FC = () => (
   <StoreProvider>
