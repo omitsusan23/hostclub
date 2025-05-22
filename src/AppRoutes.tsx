3226e2a614316ab0a2a01b885b5690ba2e95b47b
omitsusan23 <m0323replay0417@gmail.com>
2025-05-22 15:48:47 +0900

    Deploy application to Netlify

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,20 +1,22 @@
 // src/AppRoutes.tsx
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import AdminDashboard from './pages/AdminDashboard';
-import CastDashboard from './pages/CastDashboard';
+import { useStore } from './context/StoreContext';
+import StoreDashboard from './pages/StoreDashboard';
 import TableStatusPage from './pages/TableStatusPage';
 import ReservationPage from './pages/ReservationPage';
 
-const AppRoutes: React.FC = () => (
-  <Routes>
-    <Route path="/" element={<Navigate to="/dashboard" replace />} />
-    <Route path="/dashboard" element={<AdminDashboard />} />
-    <Route path="/cast" element={<CastDashboard />} />
-    <Route path="/tables" element={<TableStatusPage />} />
-    <Route path="/reservations" element={<ReservationPage />} />
-    {/* 必要に応じて他ルートも追加 */}
-  </Routes>
-);
+const AppRoutes: React.FC = () => {
+  const { stores } = useStore();
+  const defaultId = stores.length > 0 ? stores[0].id : '';
 
-export default AppRoutes;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to={`/stores/${defaultId}`} replace />} />
+      <Route path="/stores/:storeId" element={<StoreDashboard />} />
+      {/* 他の店舗固有ページは必要に応じて追加 */}
+      <Route path="/tables" element={<TableStatusPage />} />
+      <Route path="/reservations" element={<ReservationPage />} />
+    </Routes>
+  );
+};
