11160cd33aac08dcde9ded656e26fbc2da6ca109
omitsusan23 <m0323replay0417@gmail.com>
2025-05-22 15:30:19 +0900

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,19 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { StoreProvider } from './context/StoreContext';
+import { AppProvider } from './context/AppContext'; // 追加
 import Layout from './components/Layout';
 import AppRoutes from './AppRoutes';
 
 const App: React.FC = () => (
   <StoreProvider>
-    <Router>
-      <Layout>
-        <AppRoutes />
-      </Layout>
-    </Router>
+    <AppProvider> {/* AppContext でラップ */}
+      <Router>
+        <Layout>
+          <AppRoutes />
+        </Layout>
+      </Router>
+    </AppProvider>
   </StoreProvider>
 );
 
