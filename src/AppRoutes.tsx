73856e15d4a0701f502e4c728634bed514b10227
omitsusan23 <m0323replay0417@gmail.com>
2025-05-23 15:42:25 +0900

    Deploy application to Netlify

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -4,6 +4,9 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import { useStore } from './context/StoreContext';
 import TableStatusPage from './pages/TableStatusPage';
 import ReservationPage from './pages/ReservationPage';
+import Layout from './components/Layout';
+import AdminDashboard from './pages/AdminDashboard';
+import CastDashboard from './pages/CastDashboard';
 
 const AppRoutes: React.FC = () => {
   const { stores } = useStore();
@@ -12,10 +15,12 @@ const AppRoutes: React.FC = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigate to={`/stores/${defaultId}`} replace />} />
-      {/* 他の店舗固有ページは必要に応じて追加 */}
-      <Route path="/tables" element={<TableStatusPage />} />
-      <Route path="/reservations" element={<ReservationPage />} />
+      <Route path="/stores/:subdomain" element={<Layout><AdminDashboard /></Layout>} />
+      <Route path="/cast/:subdomain" element={<Layout><CastDashboard /></Layout>} />
+      <Route path="/tables" element={<Layout><TableStatusPage /></Layout>} />
+      <Route path="/reservations" element={<Layout><ReservationPage /></Layout>} />
     </Routes>
   );
 };
+
 export default AppRoutes;
