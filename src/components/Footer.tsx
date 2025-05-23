d24475074db82e08ffdbf5f1e4b075d46db4251b
omitsusan23 <m0323replay0417@gmail.com>
2025-05-23 15:38:19 +0900

    Deploy application to Netlify

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useStore } from '../context/StoreContext';
 
 const Header: React.FC = () => {
   const { currentStore } = useStore();
+
+  useEffect(() => {
+    console.log('currentStore:', currentStore);
+  }, [currentStore]);
 
   return (
     <header className="bg-white shadow p-4 flex justify-center items-center">
