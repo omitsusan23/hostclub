36948a93fd3b0cf10055f96f2b2baca751942fe0
omitsusan23 <m0323replay0417@gmail.com>
2025-05-23 15:37:54 +0900

    Deploy application to Netlify

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,31 +1,16 @@
 import React from 'react';
 import { useStore } from '../context/StoreContext';
 
-const Footer: React.FC = () => {
-  const { user } = useStore();
-  const isEmployee = user?.role === 'owner' || user?.role === 'operator';
-  const isCast = user?.role === 'cast';
+const Header: React.FC = () => {
+  const { currentStore } = useStore();
 
   return (
-    <footer className="bg-white shadow p-4 flex justify-around items-center">
-      {isEmployee && (
-        <>
-          <a href="/tables">卓状況</a>
-          <a href="/reservations">来店予約</a>
-          <a href="/casts">キャスト一覧</a>
-          <a href="/settings">設定</a>
-        </>
-      )}
-      {isCast && (
-        <>
-          <a href="/tables">卓状況</a>
-          <a href="/reservations">来店予約</a>
-          <a href="/princesses">姫一覧</a>
-          <a href="/mypage">マイページ</a>
-        </>
-      )}
-    </footer>
+    <header className="bg-white shadow p-4 flex justify-center items-center">
+      <span className="font-semibold">
+        {currentStore?.name || '店舗情報が取得できません'}
+      </span>
+    </header>
   );
 };
 
-export default Footer;
+export default Header;
