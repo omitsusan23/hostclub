1fb22902ffcedd9421794c69a1b5132403cca0bf
omitsusan23 <m0323replay0417@gmail.com>
2025-06-02 08:23:35 +0000

    main

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -22,7 +22,7 @@ const Footer: React.FC = () => {
     <footer className="fixed bottom-0 left-0 right-0 z-50 bg-white shadow px-2 pt-2 pb-[calc(2.5rem+env(safe-area-inset-bottom))] flex justify-around items-center">
       {isEmployee && (
         <>
-          <FooterButton to="/casts" icon={CastIcon} label="キャスト一覧" />
+          <FooterButton to="/casts" icon={CastIcon} label="キャスト" />
           <FooterButton to="/reservations" icon={ReservationIcon} label="来店予約" />
           <FooterButton to="/tables" icon={TableStatusIcon} label="卓状況" />
           <FooterButton to="/chat" icon={ChatIcon} label="チャット" />
