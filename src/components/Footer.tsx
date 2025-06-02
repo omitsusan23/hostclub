ecb3eb630b4455af250ffe9f8975592357f95e0c
omitsusan23 <m0323replay0417@gmail.com>
2025-06-02 08:34:32 +0000

    main

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,3 @@
-// src/components/Footer.tsx
 import React from 'react';
 import { useAppContext } from '../context/AppContext';
 import { FooterButton } from './FooterButton';
@@ -6,8 +5,8 @@ import { FooterButton } from './FooterButton';
 import CastIcon from '../assets/icons/cast.svg';
 import ReservationIcon from '../assets/icons/reservation.svg';
 import TableStatusIcon from '../assets/icons/table-status.svg';
+import ChatIcon from '../assets/icons/chat.svg';
 import SettingsIcon from '../assets/icons/settings.svg';
-import ChatIcon from '../assets/icons/chat.svg'; // チャットアイコン追加
 
 const Footer: React.FC = () => {
   const { state } = useAppContext();
@@ -19,25 +18,22 @@ const Footer: React.FC = () => {
   const isCast = user.role === 'cast';
 
   return (
-    <footer className="fixed bottom-0 left-0 right-0 z-50 bg-white shadow px-2 pt-2 pb-[calc(2.5rem+env(safe-area-inset-bottom))]">
-      <div className="mx-auto max-w-sm flex justify-between items-center">
-        {isEmployee && (
-          <>
-            <FooterButton to="/casts" icon={CastIcon} label="キャスト" />
-            <FooterButton to="/reservations" icon={ReservationIcon} label="来店予約" />
-            <FooterButton to="/tables" icon={TableStatusIcon} label="卓状況" />
-            <FooterButton to="/chat" icon={ChatIcon} label="チャット" />
-            <FooterButton to="/settings" icon={SettingsIcon} label="設定" />
-          </>
-        )}
-        {isCast && (
-          <>
-            <FooterButton to="/reservations" icon={ReservationIcon} label="来店予約" />
-            <FooterButton to="/tables" icon={TableStatusIcon} label="卓状況" />
-            <FooterButton to="/chat" icon={ChatIcon} label="チャット" />
-          </>
-        )}
-      </div>
+    <footer className="fixed bottom-0 left-0 right-0 z-50 bg-white shadow px-4 pt-2 pb-[calc(2.5rem+env(safe-area-inset-bottom))] flex justify-between items-center max-w-screen-sm mx-auto gap-x-2">
+      {isEmployee && (
+        <>
+          <FooterButton to="/casts" icon={CastIcon} label="キャスト" />
+          <FooterButton to="/reservations" icon={ReservationIcon} label="来店予約" />
+          <FooterButton to="/tables" icon={TableStatusIcon} label="卓状況" />
+          <FooterButton to="/chat" icon={ChatIcon} label="チャット" />
+          <FooterButton to="/settings" icon={SettingsIcon} label="設定" />
+        </>
+      )}
+      {isCast && (
+        <>
+          <FooterButton to="/tables" icon={TableStatusIcon} label="卓状況" />
+          <FooterButton to="/reservations" icon={ReservationIcon} label="来店予約" />
+        </>
+      )}
     </footer>
   );
 };
