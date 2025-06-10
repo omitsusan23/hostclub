cd30bc13de2de344d89eb1f2dfac5e4102b93908
omitsusan23 <m0323replay0417@gmail.com>
2025-06-10 10:08:34 +0000

    main

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import { I18nextProvider } from 'react-i18next';
 import i18n from './i18n';
 import App from './App';
 import { AppProvider } from './context/AppContext';
+import { StoreProvider } from './context/StoreContext'; // ✅ 追加
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
@@ -13,8 +14,11 @@ createRoot(document.getElementById('root')!).render(
     <I18nextProvider i18n={i18n}>
       {/* グローバルな状態管理を提供 */}
       <AppProvider>
-        <App />
+        {/* ✅ 店舗情報（StoreContext）を提供 */}
+        <StoreProvider>
+          <App />
+        </StoreProvider>
       </AppProvider>
     </I18nextProvider>
-  </StrictMode>,
+  </StrictMode>
 );
