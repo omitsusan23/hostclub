389038c61fa8db7333ba96bb42adf8be9a172936
omitsusan23 <m0323replay0417@gmail.com>
2025-06-10 07:16:11 +0000

    main

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -74,7 +74,7 @@ const Login = () => {
         placeholder="パスワード"
         value={password}
         onChange={(e) => setPassword(e.target.value)}
-        className="border px-3 py-2 mb-2 w-64 rounded"
+        className="border px-3 py-2 mb-2 w-64 rounded font-sans"
       />
 
       {error && <p className="text-red-500 text-sm mb-2">{error}</p>}
