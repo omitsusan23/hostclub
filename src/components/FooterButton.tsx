8bbe76533147a5074e43a152c79cfedefb54da34
omitsusan23 <m0323replay0417@gmail.com>
2025-06-11 07:43:45 +0000

    main

--- src/components/FooterButton.tsx
+++ src/components/FooterButton.tsx
@@ -13,7 +13,7 @@ export const FooterButton: React.FC<Props> = ({ to, icon, label }) => (
     to={to}
     className="flex flex-col items-center justify-center text-xs text-gray-700 hover:text-pink-600"
   >
-    <img src={icon} alt="" className="w-6 h-6 mb-1" />
+    <img src={icon} alt="" className="w-6 h-6 mb-2" />
     <span>{label}</span>
   </Link>
 );
