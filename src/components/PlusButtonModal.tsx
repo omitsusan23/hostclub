057cf11e72db64451c83c52db404e016d10511b8
omitsusan23 <m0323replay0417@gmail.com>
2025-07-30 20:13:25 +0900

    main

--- src/components/PlusButtonModal.tsx
+++ src/components/PlusButtonModal.tsx
@@ -60,31 +60,29 @@ export const PlusButtonModal: React.FC<PlusButtonModalProps> = ({ isOpen, onClos
         className="fixed inset-0 bg-black bg-opacity-75 z-[55]"
       />
       
-      {/* Modal content - positioned to be centered with X button */}
-      <div className="fixed left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 z-[65]">
-        {/* Menu items with black background style */}
-        <div className="bg-black rounded-lg overflow-hidden">
+      {/* Modal content - positioned above the X button */}
+      <div className="fixed bottom-[140px] left-0 right-0 z-[65] px-6">
+        {/* Menu items without background container */}
+        <div className="space-y-0">
           {menuItems.map((item, index) => (
             <button
               key={item.id}
               onClick={item.onClick}
-              className="w-full text-white hover:bg-gray-900 transition-colors block"
+              className="w-full text-white text-left block"
             >
-              <div className="px-6 py-4">
+              <div className="py-3">
                 <div className="flex items-center">
                   {/* Checkbox icon */}
-                  <div className="w-5 h-5 border-2 border-white rounded-sm flex items-center justify-center mr-3">
-                    <svg className="w-3 h-3 text-white" fill="currentColor" viewBox="0 0 20 20">
+                  <div className="w-6 h-6 border-2 border-white rounded flex items-center justify-center mr-3 flex-shrink-0">
+                    <svg className="w-4 h-4 text-white" fill="currentColor" viewBox="0 0 20 20">
                       <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
                     </svg>
                   </div>
-                  <span className="text-base font-medium whitespace-nowrap">{item.title}</span>
+                  <span className="text-lg font-bold">{item.title}</span>
                 </div>
+                {/* Bottom border */}
+                <div className="h-[2px] bg-white mt-3" />
               </div>
-              {/* Bottom border - not on last item */}
-              {index < menuItems.length - 1 && (
-                <div className="h-[1px] bg-white opacity-20 mx-4" />
-              )}
             </button>
           ))}
         </div>
