c7a8a0e7aaf2e42f597cb2221d63788977e9700a
omitsusan23 <m0323replay0417@gmail.com>
2025-07-31 17:50:32 +0900

    main

--- src/pages/PrincessPage.tsx
+++ src/pages/PrincessPage.tsx
@@ -5,10 +5,8 @@ const PrincessPage: React.FC = () => {
   return (
     <>
       <Header title="姫" />
-      {/* 統一された間隔設定 */}
-      <main className="pb-16 pt-[calc(env(safe-area-inset-top)+66px)]">
-        {/* 姫一覧ヘッダー */}
-        <header className="relative w-full h-[42px] bg-black" role="banner">
+      {/* 姫一覧ヘッダー - ヘッダー直下に配置 */}
+      <header className="relative w-full h-[42px] bg-black" role="banner">
           <div className="inline-flex items-center absolute top-2 left-[calc(50%-61px)]">
             <h1 className="relative w-fit mt-[-1.00px] [font-family:'Inter-Bold',Helvetica] font-bold text-white text-xl text-center tracking-[0] leading-[normal] whitespace-nowrap">
               姫一覧
@@ -23,6 +21,8 @@ const PrincessPage: React.FC = () => {
             type="button"
           />
         </header>
+      <main className="pb-16">
+        {/* ここに今後のコンテンツを追加 */}
       </main>
     </>
   );
