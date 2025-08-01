6c5eda1bfecf00b274acb514da80cb3de33b5c3b
omitsusan23 <m0323replay0417@gmail.com>
2025-08-01 16:06:30 +0900

    main

--- src/components/FooterButton.tsx
+++ src/components/FooterButton.tsx
@@ -12,8 +12,8 @@ type Props = {
 export const FooterButton: React.FC<Props> = ({ to, icon, label, disabled }) => {
   if (disabled) {
     return (
-      <div className="flex flex-col items-center justify-center text-xs text-gray-700 opacity-50 cursor-not-allowed">
-        <img src={icon} alt="" className="w-6 h-6 mb-2" />
+      <div className="flex flex-col items-center justify-center text-[10px] text-gray-700 opacity-50 cursor-not-allowed">
+        <img src={icon} alt="" className="w-8 h-8 mb-2" />
         <span>{label}</span>
       </div>
     );
@@ -22,9 +22,9 @@ export const FooterButton: React.FC<Props> = ({ to, icon, label, disabled }) => 
   return (
     <Link
       to={to}
-      className="flex flex-col items-center justify-center text-xs text-gray-700 hover:text-pink-600"
+      className="flex flex-col items-center justify-center text-[10px] text-gray-700 hover:text-pink-600"
     >
-      <img src={icon} alt="" className="w-6 h-6 mb-2" />
+      <img src={icon} alt="" className="w-8 h-8 mb-2" />
       <span>{label}</span>
     </Link>
   );
